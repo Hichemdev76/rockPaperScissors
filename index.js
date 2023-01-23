1709bb0a7acaeeac5534c0775364b4f4a3447389
hichem <56438155+Hichemdev76@users.noreply.github.com>
2023-01-23 19:24:58 +0100

    Add playRound() with custom outcome

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
+const gameOutcomes = {
+  rock: {
+    rock: "tie",
+    paper: "loss",
+    scissors: "win",
+  },
+  paper: {
+    rock: "win",
+    paper: "tie",
+    scissors: "loss",
+  },
+  scissors: {
+    rock: "loss",
+    paper: "win",
+    scissors: "tie",
+  },
+};
+
 function getComputerChoice() {
-    const gameInputs = ["rock", "paper", "scissors"];
-    let index = Math.floor(Math.random() * gameInputs.length);
-    return gameInputs[index];
-  }
+  const gameInputs = ["rock", "paper", "scissors"];
+  let index = Math.floor(Math.random() * gameInputs.length);
+  return gameInputs[index];
+}
+let computerSelection = getComputerChoice();
+let playerSelection = "rock";
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == computerSelection) return "It's a Tie";
+  let outcome = gameOutcomes[playerSelection][computerSelection];
+  return outcome === "win" ? "Yey you won" : "You Lost";
+}
+console.log(playRound(playerSelection,computerSelection));
